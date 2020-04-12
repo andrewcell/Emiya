c0ab97d54b325d82f7b84fa429d9296d01f7d01f
Seungyeon Choi <git@vxz.me>
2020-04-12 15:53:49 +0900

    Update Category

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'NTAG215', miibo: mibo });
 });
 
+router.get(['/category', '/category/:id'], function(req, res, next) {
+  var id = parseInt(req.params.id);
+  if (req.params.id === undefined) id = 1;
+  const mibo = JSON.parse(require('fs').readFileSync("miibo.json")).data;
+  var newData = []
+  mibo.forEach(function(ambo, index, object) {
+    if (ambo.number !== -1) {
+      if (ambo.number >= ((id-1)*100)+1 && ambo.number <= (100 * (id))) {
+        const randomKey = makeId(6);
+        downloadKey[randomKey] = {file: ambo.file, name: ambo.number + " - " + ambo.name + " - " + ambo.name_kor};
+        ambo.file = randomKey;
+        newData.push(ambo)
+      } else {
+        //object.splice(index, 1)
+      }
+    }
+  });
+
+  res.render("category", { title: "NTAG215", miibo: newData, category: id });
+});
+
 router.get("/data/:key", function(req, res) {
   const key = req.params.key;
   if (!(key in downloadKey)) {
@@ -35,7 +56,7 @@ function save(name) {
   }
   var log = JSON.parse(fs.readFileSync("log.json"));
   log[name] = (name in log) ? log[name]++ : 1;
-  fs.writeFileSync('log.json', JSON.stringify(log))
+  fs.writeFileSync('log.json', JSON.stringify(log, null ,4))
 }
 
 function makeId(length) {
