d9bda20ee5e5f5ab7c2f8b14f12a9db2db37e362
Seungyeon Choi <git@vxz.me>
2020-04-12 15:25:49 +0900

    Update recent updates

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.disable("x-powered-by");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ router.get('/', function(req, res, next) {
   const mibo = JSON.parse(require('fs').readFileSync("miibo.json")).data;
 
   mibo.forEach(function(ambo) {
-    const randomKey = makeId(6);
-    downloadKey[randomKey] = {file: ambo.file, name: ambo.number + " - " + ambo.name + " - " + ambo.name_kor};
-    ambo.file = randomKey;
+    if (ambo.number !== -1) {
+      const randomKey = makeId(6);
+      downloadKey[randomKey] = {file: ambo.file, name: ambo.number + " - " + ambo.name + " - " + ambo.name_kor};
+      ambo.file = randomKey;
+    }
   });
   res.render('index', { title: 'NTAG215', miibo: mibo });
 });
@@ -21,9 +23,21 @@ router.get("/data/:key", function(req, res) {
     return res.json({"メッセージ": "要求の形式が正しくありません。"});
   } else {
     const realFile = downloadKey[key];
-    res.download(path.resolve("data/Cards/" +realFile.file), realFile.name  + ".bin")
+    save(realFile.name);
+    res.download(path.resolve("data/Cards/" + realFile.file), realFile.name + ".bin")
   }
 });
+
+function save(name) {
+  const fs = require('fs');
+  if (!fs.existsSync("log.json")) {
+    fs.writeFileSync("log.json", "{}")
+  }
+  var log = JSON.parse(fs.readFileSync("log.json"));
+  log[name] = (name in log) ? log[name]++ : 1;
+  fs.writeFileSync('log.json', JSON.stringify(log))
+}
+
 function makeId(length) {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
