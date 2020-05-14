83e7399851b06fe7c8f8801e831e6e87ab178141
Seungyeon Choi <git@vxz.me>
2020-05-15 03:19:45 +0900

    Fix build to Sync

--- util/build.js
+++ util/build.js
@@ -7,19 +7,18 @@ try {
     // Copy front-end files
     fs.copySync('./src/public/stylesheets', './dist/public/stylesheets');
     fs.copySync('./src/public/images', './dist/public/images');
-    fs.copySync('./src/public/locales', './dist/public/locales');
     fs.copySync('./src/public/scripts/emibo.js', './dist/public/scripts/emibo.js');
     fs.copySync('./src/views', './dist/views');
     fs.copySync('./src/data', './dist/data');
 
     // Transpile the typescript files
-    childProcess.exec('tsc --build tsconfig.prod.json');
+    childProcess.execSync('tsc --build tsconfig.prod.json');
     console.log('TypeScript build complete.')
-    childProcess.exec('webpack --config ./webpack.config.prod.js', () => {
-        console.log('Webpack build complete.')
-        childProcess.exec('javascript-obfuscator ./dist/public/scripts --output ./ --compact true --self-defending true')
-        console.log('Obfuscate javascript files complete.')
-    });
+    childProcess.execSync('webpack --config ./webpack.config.prod.js');
+    console.log('Webpack build complete.')
+    childProcess.execSync('javascript-obfuscator ./dist/public/scripts --output ./ --compact true --self-defending true')
+    console.log('Obfuscate javascript files complete.')
+
 
 } catch (err) {
     console.log(err);
