c7d5be0cbb1321f91112066dd21e9616d9d36e0a
Seungyeon Choi <git@vxz.me>
2020-05-17 15:57:16 +0900

    Reduce js file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const nodemon = require('nodemon-webpack-plugin');
 const config = require('./webpack.config');
+const terserPlugin = require('terser-webpack-plugin');
 module.exports = {
     mode: 'production',
     module: config.module,
     entry: config.entry,
     resolve: config.resolve,
+    optimization: {
+        minimize: true,
+        minimizer: [new terserPlugin()]
+    },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist', 'public', 'scripts')
