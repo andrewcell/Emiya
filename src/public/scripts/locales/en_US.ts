ae06baa209904d4a69793af9a4ee9a815a07d9cb
Seungyeon Choi <git@vxz.me>
2020-05-26 12:54:28 +0900

    Add Clothes and sample display

--- src/public/scripts/locales/en_US.ts
+++ src/public/scripts/locales/en_US.ts
@@ -109,7 +109,7 @@ export default new Map([
     ['villagers.genders.female', 'Female'],
     ['villagers.my.sex', 'Genders'],
     ['villagers.nav.giftforvillagers', 'Gift for My\'s'],
-    ['villagers.nav.villagerstogift', 'Prefer Clothes'],
+    ['villagers.nav.villagerstogift', 'Prefer ClothesItem'],
     ['villagers.search.search', 'Search'],
     ['villagers.search.live', 'Live Search'],
 ]);
