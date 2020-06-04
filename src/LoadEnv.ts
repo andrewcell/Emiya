b3834d8a5e888221eb172fbe6395a7da24be6aad
Seungyeon Choi <git@vxz.me>
2020-06-04 09:18:10 +0900

    Add try ~ catch to LoadEnv

--- src/LoadEnv.ts
+++ src/LoadEnv.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import commandLineArgs from 'command-line-args';
+import logger from '@shared/Logger';
 
 // Setup command line options
 const options = commandLineArgs([
@@ -11,11 +12,16 @@ const options = commandLineArgs([
     },
 ]);
 
-// Set the env file
-const result2 = dotenv.config({
-    path: `./env/${options.env}.env`,
-});
-
-if (result2.error) {
-    throw result2.error;
+try {
+    // Set the env file
+    const result2 = dotenv.config({
+        path: `./env/${options.env}.env`,
+    });
+    if (result2.error) {
+        throw result2.error;
+    }   
+} catch (e) {
+    logger.error('env not found', e);
 }
+
+
