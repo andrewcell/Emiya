c6678319e714265cb62b0fc379f032715e25720f
Seungyeon Choi <git@vxz.me>
2020-07-01 13:00:49 +0900

    Add instruction mail function

--- src/routes/controlpanel.ts
+++ src/routes/controlpanel.ts
@@ -6,8 +6,10 @@ import path from 'path'
 import { Reader, AsnResponse, CityResponse, CountryResponse } from 'maxmind';
 import { getDataPath } from '@shared/functions'
 import { validateAdmin } from '@shared/validation';
-import User from '@shared/User';
+import User, {UserDocument} from '@shared/User';
 import moment from 'moment';
+import {Mail} from '@shared/Mail';
+import {SendGrid} from '@shared/SendGrid';
 const router = Router();
 
 router.get('/downloadlog', validateAdmin, (req: Request, res: Response) => {
@@ -36,5 +38,18 @@ router.get('/users', validateAdmin, (req, res) => {
     return res.render('users', {users, moment});
   });
 });
+
+router.get('/sendmail/:username', validateAdmin, (req, res) => {
+  const username = req.params.username;
+  User.findOne({username}, (err: never, user: UserDocument) => {
+    if (user != null) {
+      const html = `<h5>서버와 API 간의 오류로 인해 메일이 정상적으로 전송되지 않았던게 확인되어 가입 메일이 재전송되었습니다. 여전히 인증을 원한다면 이 링크를 클릭하세요: </h5><a href="https://dodo.ij.rs/admin/verify/${user.verifyHash}">https://dodo.ij.rs/admin/verify/${user.verifyHash}</a>`
+      SendGrid.send(user.email, '[DodoSeki] 서버 오류로 재전송된 가입 인증 메일입니다.', html).then(() => {
+        return res.json({})
+      });
+    }
+  })
+
+})
 
 export default router;
