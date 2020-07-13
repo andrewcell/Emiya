20ce63968bcb2779a898569282002bc2321985c2
Seungyeon Choi <git@vxz.me>
2020-07-13 15:56:52 +0900

    Update some codes

--- src/public/scripts/emiyaj/sendWithToken.ts
+++ src/public/scripts/emiyaj/sendWithToken.ts
@@ -1,5 +1,6 @@
 import Axios from "axios";
 import {emiyaJ} from "../api";
+import {l} from "../locale";
 
 export class EmiyaJ {
     private static instance: EmiyaJ;
@@ -13,9 +14,20 @@ export class EmiyaJ {
         return EmiyaJ.instance
     }
 
-    private token: string = '';
+    private token = localStorage.getItem('token');
 
-    public send(data: string, path: string): void {
-        Axios.post(emiyaJ + path, {data: data})
+    public send(data: string, path: string): Promise<string> {
+        if (this.token == null) {
+            return Promise.reject(l('emiyaj.tokenempty'))
+        }
+        return new Promise<string>((resolve, reject) => {
+            Axios.post(emiyaJ + path, {data}, {headers: {token: this.token}})
+                .then(r => {
+                    return resolve('')
+                })
+                .catch(e => {
+                    return reject('')
+                })
+        })
     }
 }
