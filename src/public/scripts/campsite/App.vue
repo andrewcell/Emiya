bf90c0ba741df6ae82d908102da40fae7e79d149
Seungyeon Choi <git@vxz.me>
2020-07-21 10:07:55 +0900

    Fix Campsite

--- src/public/scripts/campsite/App.vue
+++ src/public/scripts/campsite/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
+  <v-app>
     <Layout />
-    <v-container>
-    </v-container>
-  </div>
+    <v-container />
+  </v-app>
 </template>
 <script lang='ts'>
   import {Vue, Component} from 'vue-property-decorator';
