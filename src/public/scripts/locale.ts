0cf3a8319fa0dd2b01607bbfde684ea14b82ced6
Seungyeon Choi <git@vxz.me>
2020-07-24 15:02:21 +0900

    Move locale file to json

---
+++ src/public/scripts/locale.ts
@@ -0,0 +1,95 @@
+import ko from './locales/ko_KR.json';
+import en from './locales/en_US.json';
+import ja from './locales/ja_JP.json';
+import Cookies from 'js-cookie';
+
+const whitelist = new Map([
+    ['en_US', en],
+    ['ko_KR', ko],
+    ['ja_JP', ja]
+]);
+
+let languageConfigured = '';
+
+const setLanguage = (lang: string): void | Promise<void> => {
+    const data = whitelist.get(lang);
+    if (data == null) {
+        languageConfigured = 'en_US';
+        Cookies.set('locale', 'en_US');
+    } else {
+        languageConfigured = lang;
+        Cookies.set('locale', lang);
+    }
+    return Promise.resolve()
+}
+
+const getLanguage = (): string => {
+    return languageConfigured
+}
+
+const detectLanguage = (lang: string | null): string => {
+    switch (lang) {
+        case 'ja':
+        case 'ja-JP':
+        case 'ja_JP':
+            return 'ja_JP'
+        case 'ko':
+        case 'ko-KR':
+        case 'ko_KR':
+            return 'ko_KR'
+        case 'en':
+        case 'en-US':
+        case 'en_US':
+        default:
+            return 'en_US'
+    }
+}
+
+const missingLanguage = (): Promise<void> => {
+    return new Promise<void>((resolve) => {
+        const lang = Cookies.get('locale');
+        if (lang == null) {
+            setLanguage(detectLanguage(navigator.language));
+            return resolve();
+        }
+        if (!whitelist.has(lang)) {
+            setLanguage('en_US');
+            Cookies.set('locale', 'en_US');
+        } else {
+            if (languageConfigured === '') {
+                setLanguage(lang);
+            }
+        }
+        return resolve()
+    })
+}
+
+const l = (key: string): string => {
+    missingLanguage().then();
+    let str: string | undefined = '';
+    switch (languageConfigured) {
+        case 'ko_KR':
+            // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+            // @ts-ignore
+            str = ko[key];
+            break;
+        case 'en_US':
+            // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+            // @ts-ignore
+            str = en[key];
+            break;
+        case 'ja_JP':
+            // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+            // @ts-ignore
+            str = ja[key];
+
+    }
+    if (str == null) {
+        return '';
+
+    } else {
+        return str;
+    }
+}
+
+export {l, setLanguage, detectLanguage, getLanguage};
