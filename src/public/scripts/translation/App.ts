36163f72014074fb8009f31a9c6a52571eb15a69
Seungyeon Choi <git@vxz.me>
2020-08-31 11:09:37 +0900

    Update Translations code

--- src/public/scripts/translation/App.ts
+++ src/public/scripts/translation/App.ts
@@ -2,6 +2,11 @@ import {Vue, Component} from 'vue-property-decorator';
 import Layout from '../vuetify/Layout.vue';
 import {l} from '../locale';
 
+type localeInfo = {
+    language: string;
+    name: string;
+}
+
 @Component({
     components: {
         Layout
@@ -9,4 +14,9 @@ import {l} from '../locale';
 })
 export default class Translation extends Vue {
     l = l;
+    languages = ['kr', 'us']
+    locales: localeInfo[] = [
+        {language: 'kr', name: ''}
+    ];
+
 }
--- src/public/scripts/translation/App.vue
+++ src/public/scripts/translation/App.vue
@@ -2,12 +2,34 @@
   <v-app>
     <Layout />
     <v-container>
+      <v-text-field
+        :label="l('translations.search') + ' + Enter'"
+        outlined
+        @keyup.enter="() => {}"
+      />
       <v-card>
-        <v-text-field
-          :label="l('translations.search')"
-          outlined
-        />
+        <v-list>
+          <v-list-item
+            v-for="locale in locales"
+            :key="locale.language"
+          >
+            <v-list-item-avatar>
+              <v-icon v-text="asd" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-text="'sbsdfbsdfbsdfbfdb'" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
       </v-card>
+      <v-btn
+        rounded
+        dark
+        style="margin-top: 10px"
+        color="green"
+      >
+        {{ l('translations.manualbutton') }}
+      </v-btn>
     </v-container>
   </v-app>
 </template>
