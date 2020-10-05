a861e87165ff2065567188248d08b1630b792e52
Seungyeon Choi <git@vxz.me>
2020-10-05 16:27:10 +0900

    Fix to localhost binding

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import logger from '@shared/Logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-const host = (process.env.HOST || '0.0.0.0');
+const host = 'localhost' // (process.env.HOST || '0.0.0.0');
 app.listen(port, host, () => {
     logger.info(`Emiya listening on ${host}:${port}`);
 });
