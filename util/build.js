71233a24401ee29d0634a73ee785336146bf5af7
Seungyeon Choi <git@vxz.me>
2023-03-26 20:42:17 +0900

    Now available set AES key on build

--- util/build.js
+++ util/build.js
@@ -1,18 +1,27 @@
-const fs = require('fs-extra');
+const fsx = require('fs-extra');
+const fs = require('fs');
 const childProcess = require('child_process');
-
+const dotenv = require('dotenv');
+const encFile = 'src/public/scripts/encryption/AES.ts';
 try {
 
     // Remove current build
-    fs.removeSync('./dist/');
+    fsx.removeSync('./dist/');
     // Copy front-end files
-    fs.copySync('./src/public/stylesheets', './dist/public/stylesheets');
-    fs.copySync('./src/public/images', './dist/public/images');
-    fs.copySync('./src/public/manifests', './dist/public/manifests');
-    fs.copySync('./src/public/apple-touch-icon.png', './dist/public/apple-touch-icon.png');
-    fs.copySync('./src/views', './dist/views');
-    fs.copySync('./src/data', './dist/data');
-
+    fsx.copySync('./src/public/stylesheets', './dist/public/stylesheets');
+    fsx.copySync('./src/public/images', './dist/public/images');
+    fsx.copySync('./src/public/manifests', './dist/public/manifests');
+    fsx.copySync('./src/public/apple-touch-icon.png', './dist/public/apple-touch-icon.png');
+    fsx.copySync('./src/views', './dist/views');
+    fsx.copySync('./src/data', './dist/data');
+    // Set Encryption key to front-end
+    const conf = dotenv.config({
+        path: './env/production.env',
+    });
+    let file = fs.readFileSync(encFile, 'utf8');
+    const fileOriginal = file + '';
+    file = file.replace(/Put_your_own_key_here/g, process.env.AESKEY);
+    fs.writeFileSync(encFile, file, 'utf8');
     // Transpile the typescript files
     childProcess.execSync('tsc --build tsconfig.prod.json');
     console.log('TypeScript build complete.')
@@ -20,8 +29,8 @@ try {
     console.log('Webpack build complete.')
     childProcess.execSync('javascript-obfuscator ./dist/public/scripts --output ./ --compact true --string-array true --self-defending false --control-flow-flattening false --debug-protection false  --disable-console-output true --split-strings true --target browser-no-eval')
     console.log('Obfuscate javascript files complete.')
-    require('fs').writeFileSync('buildtime.txt', Date.now().toString());
-
+    fs.writeFileSync('buildtime.txt', Date.now().toString());
+    fs.writeFileSync(encFile, fileOriginal, 'utf8');
 } catch (err) {
     console.log(err);
 }
